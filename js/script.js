b6e1d13b085a62f5066c6d58632dcb97af7adbc9
Lozweb <aelozweb@gmail.com>
2022-07-24 12:29:53 +0200

    new bg, add onload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ import {Engine} from "./Engine.js"
 import {Screen} from "./Screen.js"
 import {StarsGenerator} from "./StarsGenerator.js"
 
-var screen = new Screen('bgAnim')
-var stars = new StarsGenerator(screen.width, screen.heigth, 'left')
 
-var engine = new Engine(screen, stars)
-engine.InitScreen()
-var start = setInterval(engine.draw.bind(engine), 33)
+window.onload = init
+
+function init(){
+    var screen = new Screen('bgAnim')
+    var stars = new StarsGenerator(screen.width, screen.heigth, 'left')
+    
+    var engine = new Engine(screen, stars)
+    engine.InitScreen()
+    var start = setInterval(engine.draw.bind(engine), 33)
+}
